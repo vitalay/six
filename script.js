66a7dcf46b0272960f434046fe2914ebfbb4a18f
vitalya <vitalay.efimov@gmail.com>
2025-04-27 17:39:14 +0500

    Task 2 С помощью вложенных циклов, нарисуйте строку:
    1
    *_*_*_
    2
    *_*_*_
    3
    *_*_*_
    Решить задачу с помощью вложенных циклов. Внешний цикл выводит цифры и перенос строки br, , второй звездочки, знак подчеркивания и знак переноса.

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ document.querySelector('.btn').addEventListener('click', function t1() {
 })
 
 
+document.querySelector('.btn2').addEventListener('click', function t2() {
+  let out = document.querySelector('.out-2')
+
+  for (let i = 1; i <= 3; i++) {
+    out.innerHTML += i + "<br>";
+    for (let k = 0; k < 10; k++) {
+      out.innerHTML += '*_';
+    }
+    out.innerHTML += '<br>';
+  }
+})
+
